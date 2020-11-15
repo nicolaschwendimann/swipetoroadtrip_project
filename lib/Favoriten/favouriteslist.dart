6abb3c39decd2b2c6059b6fe4235f7317099b694
blackrabbit93 <44777440+blackrabbit93@users.noreply.github.com>
2020-11-15 13:20:28 +0100

    ADD: favouriteslist.dart

--- lib/Favoriten/favouriteslist.dart
+++ lib/Favoriten/favouriteslist.dart
@@ -1,3 +1,5 @@
+/*
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -16,3 +18,5 @@ class FavouritesList extends StatefulWidget {
     
   }
 }
+
+*/
